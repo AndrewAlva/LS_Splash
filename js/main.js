bdc3560e1bd24230787a68c2c86c8123b7ab209a
Alejandro Alvarado <alexconh@hotmail.com>
2017-05-21 22:37:59 -0500

    Starting to work on changing quotes

---
+++ js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+	console.log("jQuery ready puto");
+
+	var quotes = ["Anything<br>In Life<br>Can Be<br>Achieved", "There Is<br>Always A<br>Strategy<br>For It", "Connecting<br>The Dots", "Amazing<br>Things<br>Are<br>Coming", "Site Under<br>Construction"];
+
+	console.log("Quotes: " + quotes);
+});
